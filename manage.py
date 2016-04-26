6d5af24bc696e52c67afbecf3c66e5aedd17232a
ahdrage <alf@dra.ge>
2016-04-26 15:31:18 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,2 +1,5 @@
 from app import create_app #TODO: Make a create_app function
 from flask.ext.script import Manager
+
+app = create_app('default') # Note: pass an argument so we can have a few different configurations, for now we'll use default, and come back later
+manager = Manager(app)
