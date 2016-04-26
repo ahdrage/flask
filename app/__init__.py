f60e8a1d9b639da979a63bc1934d92dacc72d157
ahdrage <alf@dra.ge>
2016-04-26 20:04:06 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
-from app.blueprints.main import main as main_blueprint
-app.register_blueprint(main_blueprint)
+
 
 
 db = SQLAlchemy()
@@ -10,6 +9,8 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    from app.blueprints.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
     return app
 
 
