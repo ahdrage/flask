58da890d1effb9dbbdc23f040227005a4e0c75a7
ahdrage <alf@dra.ge>
2016-04-26 21:18:48 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
+from app import db
 
+class Listing(db.Model):
+    __tablename__ = 'listing'
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.Integer)
+    week_number = db.Column(db.Integer)
+    artist = db.Column(db.String)
+    title = db.Column(db.String)
+    spotify_url = db.Column(db.String)
+    spotify_id = db.Column(db.String)
+    image = db.Column(db.String)
