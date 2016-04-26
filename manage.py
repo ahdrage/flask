c456d830fdbbd47032b07414a423727076527e25
ahdrage <alf@dra.ge>
2016-04-26 15:32:34 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,3 +3,7 @@ from flask.ext.script import Manager
 
 app = create_app('default') # Note: pass an argument so we can have a few different configurations, for now we'll use default, and come back later
 manager = Manager(app)
+
+
+if __name__ == '__main__':
+    manager.run()
