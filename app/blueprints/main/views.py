7c3dde1e49e206526fa3570815b6369af27fd8cb
ahdrage <alf@dra.ge>
2016-04-26 21:57:58 +0200

    Update views.py

--- app/blueprints/main/views.py
+++ app/blueprints/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, redirect, url_for
 from . import main
 from app import db
+from app.models import Listing
+from .forms import UpdateForm
 
 @main.route('/')
 def home():
