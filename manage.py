a61749a58be287a7bbee602bd4169ed2bfd67a3e
ahdrage <alf@dra.ge>
2016-04-26 23:02:16 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
-from app import create_app #TODO: Make a create_app function
-from flask.ext.script import Manager
+from app import create_app, db
+from flask.ext.migrate import Migrate, MigrateCommand
+
 
 app = create_app('default') # Note: pass an argument so we can have a few different configurations, for now we'll use default, and come back later
 manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
